21150722ceb3ac01438e313e8b269863b34dad34
SamuelTD04 <shimumach529@gmail.com>
2024-09-17 22:38:39 -0300

    data alignment wizardry

--- ex037.c
+++ ex037.c
@@ -6,8 +6,22 @@ typedef struct {
     int z;
 }Point;
 
+typedef struct {
+    char x;
+    double y;
+    int z;
+}Point1;
+
+typedef struct {
+    char x;
+    int z;
+    double y;
+}Point2;
+
 int main() {
     Point point;
+    Point1 point1;
+    Point2 point2;
     printf("size of x: %d\n", sizeof(point.x)); //output: 1
     printf("size of y: %d\n", sizeof(point.y)); // output: 2
     printf("size of z: %d\n", sizeof(point.z)); // output: 4
@@ -16,9 +30,17 @@ int main() {
     printf("size of point: %d\n", sizeof(point));
     // basically, the variables where stored in addresses that are multiple of their natural sizes
     // the point struct is all the region that contains the variables from start to end, including the paddings
+    // the above is true because a struct is gonna be aligned the largest type alignment requirement (in our case, 4 B)
     printf("address of x: %p\n", &point.x);
     printf("address of y: %p\n", &point.y);
     printf("address of z: %p\n", &point.z);
     printf("address of point: %p\n", &point);
+
+    printf("=========================\n");
+    // the size of the next struct should be 13 bytes, but its actually 24!!!
+    printf("size of point1: %d\n", sizeof(point1));
+    // this is literally the same struct as above, but with a different variable declaration order
+    printf("size of point2 %d\n", sizeof(point2));
+    // see? lot better, huh? Always be careful with variable declarations when working with limited memory
     return 0;
 }
